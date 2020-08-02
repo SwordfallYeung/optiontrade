c23e1a1ed7dda57f373af164517a54c880b230e0
swordfall <2506597416@qq.com>
2020-08-02 23:18:11 +0800

    添加港美股指数注释

--- com/swordfall/trade/hk/HangSengIndexesDaily.py
+++ com/swordfall/trade/hk/HangSengIndexesDaily.py
@@ -3,7 +3,15 @@ from com.swordfall.trade.common.CommonIndexesDaily import CommonIndexesDaily
 common_indexes_daily = CommonIndexesDaily()
 
 def get_hk_hang_seng_index_daily(start_date, end_date):
-   common_indexes_daily.get_index_daily(country="香港", index_name="恒生指数", start_date=start_date, end_date=end_date, method_name="get_hk_hang_seng_index_daily")
+    '''
+    获取港股恒生指数指定日期内的每天行情
+    :param start_date: 开始日期
+    :param end_date: 结束日期
+    :return:
+    '''
+    common_indexes_daily.get_index_daily(country="香港", index_name="恒生指数", start_date=start_date, end_date=end_date, method_name="get_hk_hang_seng_index_daily")
+
+
 
 if __name__ == '__main__':
     get_hk_hang_seng_index_daily("2020-01-01","2020-08-02")
--- com/swordfall/trade/us/UsIndexesDaily.py
+++ com/swordfall/trade/us/UsIndexesDaily.py
@@ -3,6 +3,12 @@ from com.swordfall.trade.common.CommonIndexesDaily import CommonIndexesDaily
 common_indexes_daily = CommonIndexesDaily()
 
 def get_us_indexes_daily(index_name, start_date, end_date):
+    '''
+    获取美股道琼斯指数、标普500指数、纳斯达克综合指数指定日期内的每天行情
+    :param start_date: 开始日期
+    :param end_date: 结束日期
+    :return:
+    '''
     common_indexes_daily.get_index_daily(country="美国", index_name=index_name, start_date=start_date, end_date=end_date,
                                          method_name="get_us_indexes_daily" +"_" + index_name)
 
