a2ea3fd4932d68e0a963ced33722762b2f8d48e4
swordfall <2506597416@qq.com>
2020-08-06 00:52:41 +0800

    完善每天港股恒生指数的获取时间

--- com/swordfall/utils/CommonUtils.py
+++ com/swordfall/utils/CommonUtils.py
@@ -20,6 +20,30 @@ class CommonUtils:
         china_time = datetime.datetime.now().date()
         return china_time
 
+    def get_china_hk_today_time(self):
+        '''
+        获取中国区香港时间
+        :return:
+        '''
+        now_time = datetime.datetime.now()
+        is_weekday = now_time.weekday()
+        if is_weekday == 5:
+            now_time = (now_time - datetime.timedelta(hours=24)).date()
+            return now_time
+
+        if is_weekday == 6:
+            now_time = (now_time - datetime.timedelta(hours=48)).date()
+            return now_time
+
+        hour = now_time.hour
+        min = now_time.minute
+        if hour < 9 or (hour > 9 and min < 30):
+            now_time = (now_time - datetime.timedelta(hours=24)).date()
+            return now_time
+        if hour > 9 and min >= 30:
+            china_time = now_time.date()
+            return china_time
+
     def get_us_today_time(self):
         '''
         获取美国区时间
@@ -28,3 +52,8 @@ class CommonUtils:
         china_time = datetime.datetime.now()
         us_time = (china_time - datetime.timedelta(hours=12)).date()
         return us_time
+
+if __name__ == '__main__':
+    st = CommonUtils()
+    date = st.get_china_hk_today_time()
+    print(date)
