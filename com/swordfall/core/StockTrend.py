b7c2a9e1cdb640c9e15c5ff07e3ad4524cc51b5c
swordfall <2506597416@qq.com>
2020-08-06 00:53:36 +0800

    添加恒生指数趋势的简单判断和平均数判断

--- com/swordfall/core/StockTrend.py
+++ com/swordfall/core/StockTrend.py
@@ -7,7 +7,14 @@ class StockTrend:
         self.commonStockService = CommonStockService()
 
     def index_up_trend(self, index_name):
-        index_daily = self.commonStockService.select_batch("恒生指数", "2020-07-01", "2020-08-04")
+        index_daily = self.commonStockService.select_batch("恒生指数", "2020-07-01", "2020-08-05")
+        self.simple_judge(index_daily)
+        print("---------------------------------------")
+        self.average_judge(index_daily)
+
+
+    def simple_judge(self, index_daily):
+        print("simple test:")
         for index in index_daily:
             date = index['date']
             open = index['open']
@@ -17,7 +24,20 @@ class StockTrend:
                 print(date, open, close, "up")
             if close < open:
                 print(date, open, close, "down")
-            #print(index)
+
+    def average_judge(self, index_daily):
+        print("average test:")
+        pre_index = 0
+        for index in index_daily:
+            date = index['date']
+            open = index['open']
+            close = index['close']
+            average = (open + close) / 2
+            if average > pre_index:
+                print(date, average, "up")
+            if average < pre_index:
+                print(date, average, "down")
+            pre_index = average
 
 if __name__ == '__main__':
     st = StockTrend()
