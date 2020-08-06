dd178fc87a23e3ec385466253eebac5839f16d31
swordfall <2506597416@qq.com>
2020-08-07 01:01:39 +0800

    查询最新连跌或连涨的次数

--- com/swordfall/core/StockTrend.py
+++ com/swordfall/core/StockTrend.py
@@ -10,7 +10,9 @@ class StockTrend:
         index_daily = self.commonStockService.select_batch("恒生指数", "2020-07-01", "2020-08-06")
         self.simple_judge(index_daily)
         print("---------------------------------------")
-        self.average_judge(index_daily)
+        #self.average_judge(index_daily)
+
+        self.calculate_nearby_daily_status(index_daily)
 
 
     def simple_judge(self, index_daily):
@@ -38,6 +40,33 @@ class StockTrend:
             if average < pre_index:
                 print(date, average, "down")
             pre_index = average
+
+    def calculate_nearby_daily_status(self, index_daily):
+        print("calculate_nearby_daily_status test:")
+
+        calculate_status = {'status':'', 'count':0}
+        for index in index_daily:
+            date = index['date']
+            open = index['open']
+            close = index['close']
+
+            status = calculate_status['status']
+
+            if close > open:
+                
+                if status is not 'up':
+                    calculate_status['status'] = 'up'
+                    calculate_status['count'] = 1
+                if status is 'up':
+                    calculate_status['count'] = calculate_status['count'] + 1
+            if close < open:
+
+                if status is not 'down':
+                    calculate_status['status'] = 'down'
+                    calculate_status['count'] = 1
+                if status is 'down':
+                    calculate_status['count'] = calculate_status['count'] + 1
+        print(calculate_status)
 
 if __name__ == '__main__':
     st = StockTrend()
